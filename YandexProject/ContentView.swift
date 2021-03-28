0029e061677b58879ee48d1a90ea9c18e8b7148c
Marfa Lamova <invisible.kichi@gmail.com>
2021-03-29 00:01:31 +0600

    Initial Commit

---
+++ YandexProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YandexProject
+//
+//  Created by Marfa Lamova on 3/29/21.
+//  Copyright © 2021 Madina Saparbayeva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
